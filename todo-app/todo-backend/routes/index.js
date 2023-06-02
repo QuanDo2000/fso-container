2f21f34eeab0ba417348a1a5bd93fd2e239846ed
Quan Do <minhquand3@gmail.com>
2023-06-02 10:52:41 +0700

    Exercise 12.11

--- todo-app/todo-backend/routes/index.js
+++ todo-app/todo-backend/routes/index.js
@@ -1,18 +1,29 @@
 const express = require('express');
+const redis = require('redis');
 const router = express.Router();
 
-const configs = require('../util/config')
+const configs = require('../util/config');
+const { getAsync } = require('../redis');
 
-let visits = 0
+let visits = 0;
 
 /* GET index data. */
 router.get('/', async (req, res) => {
-  visits++
+  visits++;
 
   res.send({
     ...configs,
-    visits
+    visits,
   });
+});
+
+router.get('/statistics', async (_, res) => {
+  const tempTodos = await getAsync('added_todos');
+  if (!tempTodos) {
+    res.send({ added_todos: 0 });
+    return;
+  }
+  res.send({ added_todos: parseInt(tempTodos) });
 });
 
 module.exports = router;
